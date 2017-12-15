9de57f999cf52926a98c3cd42391e4a550c55ef2
Anna Vos <anna-h.vos@hotmail.com>
2017-12-15 09:15:11 +0100

    automatically scroll to bottom if user is near bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 var socket = io();
 
+function scrollToBottom() {
+    // selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    // heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        console.log('should scroll');
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 // use regular functions for cross platform compatibility
 socket.on('connect', function () {
     console.log('connected to server');
@@ -25,6 +43,7 @@ socket.on('newMessage', function (message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -47,6 +66,7 @@ socket.on('newLocationMessage', function (message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 })
 
 $('#message-form').on('submit', function (e) {
